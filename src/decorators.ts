51fb2c61508cf0907760be8643558ebccbca5fc0
Tareq Hossain <xtrinsic96@gmail.com>
2025-01-12 04:27:55 +0600

    update app service decorator

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,6 +1,16 @@
 import { Service as _service } from "typedi";
 import container, { registerService } from "./container";
-export const AppService = _service;
+export function AppService(target: any):void;
+export function AppService():any;
+export function AppService(target?: any) {
+    if (target) {
+         _service()(target);
+    } else {
+        return function (tg: any) {
+            _service()(tg)
+        }
+    }
+}
 
 export * from "./controller";
 export * from "./route-methods";
