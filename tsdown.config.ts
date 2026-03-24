1686c4b1568b1683f34d2445c611e82fc7a2b565
Tareq Hossain <xtrinsic96@gmail.com>
2026-03-24 11:32:28 +0600

    feat: fix type decleartion isssue with tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
-  dts: true,
+  format: ["esm", "cjs"],
   clean: true,
   sourcemap: true,
   treeshake: true,
